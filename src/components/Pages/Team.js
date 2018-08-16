54377008b4aa487f40bc380a96f7a433f6cea566
Steven Shen <steeve.shen@gmail.com>
2018-08-15 20:14:50 -0400

    add build gitignore

--- src/components/Pages/Team.js
+++ src/components/Pages/Team.js
@@ -16,6 +16,7 @@ export const Team = () => {
             </div>
             <img className="card-img-top portrait rounded" src="images/steven.png" alt="Steven Shen"/>
             <div className="card-body">
+              <p class="card-text">Software Engineer (Toronto, Canada)</p>
               <ul className="list-inline">
                 <li className="list-inline-item"><SocialIcon url="https://www.linkedin.com/in/steeveshen/" /></li>
                 <li className="list-inline-item"><SocialIcon url="https://github.com/rockdinosaur" /></li>
@@ -31,6 +32,7 @@ export const Team = () => {
             </div>
             <img className="card-img-top portrait rounded" src="images/max.png" alt="Max Appleton"/>
             <div className="card-body">
+              <p class="card-text">Software Engineer (San Francisco, USA)</p>
               <ul className="list-inline">
                 <li className="list-inline-item"><SocialIcon url="https://www.linkedin.com/in/max-appleton/" /></li>
                 <li className="list-inline-item"><SocialIcon url="https://github.com/maxiappleton" /></li>
@@ -46,6 +48,7 @@ export const Team = () => {
             </div>
             <img className="card-img-top portrait rounded" src="images/andrew.png" alt="Andrew Houston-Floyd"/>
             <div className="card-body">
+              <p class="card-text">Software Engineer (New York City, USA)</p>
               <ul className="list-inline">
                 <li className="list-inline-item"><SocialIcon url="https://www.linkedin.com/in/andrew-houston-floyd" /></li>
                 <li className="list-inline-item"><SocialIcon url="https://github.com/houstonfloyd" /></li>
