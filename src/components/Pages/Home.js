b4dd8a3dabae40886af1211fbdb9f3d40190ec52
Max Appleton <maxiappleton@gmail.com>
2018-08-15 19:01:49 -0500

    rework home page

--- src/components/Pages/Home.js
+++ src/components/Pages/Home.js
@@ -3,17 +3,46 @@ import React from 'react';
 export const Home = () => {
   return (
     <div className="text-center">
-      <div className="jumbotron jumbotron-fluid">
+      <div className="jumbotron jumbotron-fluid pb-3 mb-0">
         <div className='container'>
           <h1 className="display-4">
-            <img className="card-img-top w-75" src="images/logo_full.png" alt="turtleDB"/>
+            <img className="card-img-top w-75" src="images/logo_full.png" alt="turtleDB" />
           </h1>
           <p className="lead">An open source promise-based wrapper for IndexedDB with syncing and conflict resolution.</p>
           <hr className="my-4"></hr>
-          <p>Currently supported back-end databases: MongoDB</p>
-          <p className="lead">
-            <a className="btn btn-success btn-lg" href="#" role="button">Get turtleDB</a>
-          </p>
+          <div className="download-links d-flex justify-content-around">
+            <div className="turtle-links">
+              <p><span className="font-weight-bold">turtleDB</span> for the front-end:</p>
+              <div className="link-buttons d-flex flex-wrap justify-content-center">
+                <a className="btn btn-success btn-lg" href="#" role="button"><i className="fas fa-download"></i></a>
+                <a className="btn btn-danger btn-lg icon-link" href="#"><i className="fab fa-npm fa-2x"></i></a>
+                <a className="btn btn-light btn-lg icon-link" href="#"><i className="fab fa-github fa-2x"></i></a>
+              </div>
+            </div>
+            <div className="tortoise-links">
+              <p><span className="font-weight-bold">tortoiseDB</span> for the back-end:</p>
+              <div className="link-buttons d-flex flex-wrap justify-content-center">
+                <a className="btn btn-success btn-lg" href="#" role="button"><i className="fas fa-download"></i></a>
+                <a className="btn btn-danger btn-lg icon-link" href="#"><i className="fab fa-npm fa-2x"></i></a>
+                <a className="btn btn-light btn-lg icon-link" href="#"><i className="fab fa-github fa-2x"></i></a>
+              </div>
+              <p className="subtext">Currently supported back-end databases: MongoDB</p>
+            </div>
+          </div>
+          <hr className="my-4"></hr>
+          <div className="learn-more">
+            <p>Learn More</p>
+            <i className="fa fa-chevron-down fa-2x" aria-hidden="true"></i>
+          </div>
+        </div>
+      </div>
+      <div className="section about">
+        <div className="d-flex justify-content-center">
+          <blockquote className="blockquote">
+            <p>TurtleDB is a framework for developers to build offline-first, collaborative web applications. It provides a user-friendly API for developers to empower their apps with in-browser storage, effective server synchronization, document versioning, and flexible conflict resolution for any document data.</p>
+            <p>Web applications will work seamlessly online or offline and developers can leave the backend to TurtleDB - it will handle all data synchronization and conflict resolution between users.</p>
+            <p>Works with MongoDB out of the box, with a flexible adapter architecture that can support most NoSQL databases.</p>
+          </blockquote>
         </div>
       </div>
       <div className="container">
@@ -23,7 +52,7 @@ export const Home = () => {
               <div className="card-header">
                 <h5 className="card-title">Offline-First</h5>
               </div>
-              <img className="card-img-top center" src="images/1.png" alt="Offline-First"/>
+              <img className="card-img-top center" src="images/1.png" alt="Offline-First" />
               <div className="card-body">
                 <p className="card-text">Queries are made to your local in-browser storage first.</p>
               </div>
@@ -35,7 +64,7 @@ export const Home = () => {
               <div className="card-header">
                 <h5 className="card-title">Synchronization</h5>
               </div>
-              <img className="card-img-top center" src="images/2.png" alt="Synchronization"/>
+              <img className="card-img-top center" src="images/2.png" alt="Synchronization" />
               <div className="card-body">
                 <p className="card-text">Syncs your data to the back-end when you have internet.</p>
               </div>
@@ -47,7 +76,7 @@ export const Home = () => {
               <div className="card-header">
                 <h5 className="card-title">Conflict Resolution</h5>
               </div>
-              <img className="card-img-top center" src="images/3.png" alt="Conflict Resolution"/>
+              <img className="card-img-top center" src="images/3.png" alt="Conflict Resolution" />
               <div className="card-body">
                 <p className="card-text">Multiple people can work on the same document without overwriting each other's changes.</p>
               </div>
--- src/index.js
+++ src/index.js
@@ -2,14 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-import Bootstrap from 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap/dist/css/bootstrap.css'
 import "./assets/styles/index.css"
 import "./assets/styles/bass.css"
 import './assets/styles/style.css'
 import './assets/styles/github.css'
 import './assets/styles/split.css'
 import './assets/styles/docs.css'
-
-
+import './../node_modules/@fortawesome/fontawesome-free/css/all.css'
 
 ReactDOM.render(<App />, document.getElementById('root'));
