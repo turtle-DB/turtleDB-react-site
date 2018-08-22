2a0334315ebeead4408734e25ed4f583f8ec6b70
houstonfloyd <andrew.houstonfloyd@gmail.com>
2018-08-21 20:30:17 -0400

    Add images and start editing browser storage section

--- src/components/Pages/AboutSections/InBrowserStorage.js
+++ src/components/Pages/AboutSections/InBrowserStorage.js
@@ -11,36 +11,35 @@ const InBrowserStorage = () => {
   return (
     <div>
       <h2 id='in-browser-storage'>In-Browser Storage</h2>
+
+      <h3 id="storage-options">Storage Options</h3>
       <p>
         If you want to store documents in the browser, there are 3 options:
         LocalStorage, WebSQL, and IndexedDB (IDB).
       </p>
-      <h5>LocalStorage</h5>
+      <h4>LocalStorage</h4>
       <p>
         A key value object store with a maximum storage limit of
         only 5 MB. LocalStorage operations are performed synchronously
          via the global `window` object in the browser. <Citation
            url='https://developer.mozilla.org/en-US/docs/Web/API/Storage/LocalStorage'
            creator='Mozilla Developer Network'
-           creationDate='May 9, 2018'
-           contributingOrganization='MDN'
            title='LocalStorage'
          />With its low limit and
          synchronous behavior, it is only good for storing small amounts of data
          in small chunks at a time.
       </p>
-      <h5>WebSQL</h5>
+
+      <h4>WebSQL</h4>
       <p>
         WebSQL uses a SQLite variation for local data storage and its original
         goal was to enable web developers to query the database using a syntax
          very similar to traditional SQL.   <Citation
               url='https://en.wikipedia.org/wiki/Web_SQL_Database'
               creator='Wikipedia'
-              creationDate='July 31, 2018'
-              contributingOrganization='Wikipedia'
               title='Web SQL Database'
-            /> However, in 2010 the W3C failed to come
-         to a consensus on how to continue developing it in a standardized manner
+            /> However, in 2010, the W3C failed to come
+         to a consensus on how to continue developing it in a standardized manner,
          so all major browsers dropped support for it before it could become a W3C
          recommendation. It has since been deprecated by all browsers and is no longer
          being developed.
@@ -64,54 +63,66 @@ const InBrowserStorage = () => {
         most browser contexts, even WebWorkers and ServiceWorkers.
       </p>
       <p>
-        However, IDB’s API has some issues which contributes to its lack of popularity.
-        Multiple async operations are needed to perform even the  simplest of tasks.
+        However, the IDB API has some issues which contributes to its lack of popularity.
+        Multiple async operations are needed to perform even simple queries.
         A typical read query requires several async calls; opening a connection, sending
         a request to the store, and then capturing the returned values in a callback function.
       </p>
 
-      <img/>
+      <img className="w-100" src="../images/browser_storage/async-queries.png" />
 
       <p>
         Furthermore, events fire on objects that are only available for the duration of the IDB connection.
         These objects disappear as soon as the connection closes.
       </p>
+
       <p>
-        Given this specification, IDB is not the simplest API to work with. Even according to Mozilla,
-        the primary backer and developer IDB, has stated:
+        A separate set of requests, objects, and events is needed for making schema
+        changes, defining indexes or running cursors.
+      </p>
+
+      <p>
+        Given this specification, IDB is not the simplest API to work with. According to Mozilla,
+        the primary backer and developer of IDB:
       </p>
       <blockquote className="blockquote mb-0">
         <hr></hr>
-        <p>“IndexedDB API is powerful, but may seem too complicated for simple cases.”
+        <p>“The IndexedDB API is powerful, but may seem too complicated for simple cases.”
           <Citation
             url='https://developer.mozilla.org/en-US/docs/Web/API/IndexedDB_API'
             creator='Mozilla Developer Network'
-            creationDate='June 14, 2018'
-            contributingOrganization='MDN'
             title='IndexedDB API'
           />
         </p>
         <hr></hr>
       </blockquote>
       <p>
-        The lack of popularity for offline-first applications is unsurprising.
-        That is why turtleDB offers a promise-based wrapper that <Link to='/API'>condenses native IDB queries
-        down to one line.</Link>
+        Given this admission, the lack of popularity for offline-first applications is unsurprising.
+        turtleDB therefore has a promise-based wrapper that <Link to='/API'>
+        condenses native IDB queries down to one line.</Link>
       </p>
-      <h3 id="turtledb-promise-api">turtleDB’s Promise API</h3>
+
+      <h3 id="wrapping-idb-with-promises">Wrapping IDB with Promises</h3>
       <p>
         Without Promises, developers have to rely on nested callback functions.
-        Callbacks can get extremely messy (i.e. ‘callback hell’), and the JavaScript
-        community has embraced Promises as a much simpler and elegant solution for dealing
-        with asynchronous code.
+        Callbacks can get extremely messy (i.e. ‘callback hell’), and the
+        JavaScript community has embraced Promises as a much simpler and
+        elegant solution for dealing with asynchronous code.
       </p>
+
       <p>
-        Promises allow us to control the execution flow of async calls. turtleDB abstracts
-        away all the complexities of asynchronous IDB code to instead expose a more familiar
-        <Link to='/API'> Promise-based API</Link> for the developer. The below code snippets
-        show how much code we save a developer from writing when using turtleDB. Both open a
-        connection to IDB and insert a new document.
+        Promises allow us to control the execution flow of async calls.
+        turtleDB abstracts away all the complexities of asynchronous IDB code
+        to instead expose a more familiar promise-based API for the developer.
       </p>
+
+      <p>
+        The below code snippets show how much code is handled for the developer
+        in turtleDB. Both open a connection to IDB and insert a new document. 
+      </p>
+
+      <h3 id="turtledb-promise-api">turtleDB’s Promise API</h3>
+
       <p>
         In cases where sequential execution matters, database operations must be kept atomic.
         For these cases, turtleDB uses a “Promise chain” where the operation only ends once
